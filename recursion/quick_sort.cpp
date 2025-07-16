172b93a7ff66fa369f73611312a830f3c0556b50
ManasCodez <manasmailsss@gmail.com>
2025-07-16 19:58:33 +0530

    quick sort

---
+++ recursion/quick_sort.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void quick_sort(vector<int>&nums, int pivot){
+
+}
+
+int main(){
+    vector<int>nums={2,5,2,1,5,7,8,5,3,9,8,9};
+    int pivot = nums.size()-1;
+    quick_sort(nums,pivot);
+}
