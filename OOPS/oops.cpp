cf5608162d8e4df840293609994109410c1000e5
ManasCodez <manasmailsss@gmail.com>
2025-07-28 19:14:01 +0530

    update

--- OOPS/oops.cpp
+++ OOPS/oops.cpp
@@ -29,7 +29,7 @@ class  teacher{
             salary = sal;
         }
          teacher(teacher& obj){//custom copy constructor
-            cout << "i am custom copy constructor\n";
+            
             this->name = obj.name;
             this->dept= obj.dept;
             this->subject= obj.subject;
@@ -76,12 +76,79 @@ class  teacher{
 
 
 
+// class student{ // shallow copy
+//     public:
+//     string name;
+//     double* cgpaptr; 
+//     student(string name, double cgpa){
+//         this->name=name;
+//         cgpaptr = new double;
+//         *cgpaptr = cgpa;
+//     }
+//     student(student &obj){
+//         this->name=obj.name;
+        
+//         this->cgpaptr = obj.cgpaptr;
+//     }
+//     void getinfo(){
+//         cout << "Student name: " << name<<endl;
+//         cout << "CGPA: " << *cgpaptr << endl;
+//     }
+// };
 
-int main(){
+
+class deepstudent{ // deep copy
+    public:
+    string name;
+    double* cgpaptr; 
+    deepstudent(string name, double cgpa){
+        this->name=name;
+        cgpaptr = new double; //adding new double to allocate new memory
+        *cgpaptr = cgpa;//changes value of the new memory address to the required one. 
+    }
+
+
+    deepstudent(deepstudent &obj){
+        this->name=obj.name;
+        cgpaptr = new double;
+        *cgpaptr = *(obj.cgpaptr);
+    }
+
+    ~deepstudent(){
+        cout << "Hi I am destrutor!";
+    }
+    void getinfo(){
+        cout << "Student name: " << name<<endl;
+        cout << "CGPA: " << *cgpaptr << endl;
+    }
+};
+
+
+
+
+// int main(){
     
-    teacher a1("manas", "CS","computer",100000); // constructor call
-    // teacher a2(a1); //default copy constructor
-    teacher t2(a1);//custom copy constructor
-    cout << t2.name;
-    return 0;
+//     teacher a1("manas", "CS","computer",100000); // constructor call
+//     // teacher a2(a1); //default copy constructor
+//     teacher t2(a1);//custom copy constructor
+    
+ 
+//     student s1("Manas",8.8);
+//     student s2(s1); // copy constructor (shaloow copy)
+//     s1.getinfo();
+//     *(s2.cgpaptr) = 9;
+//     s1.getinfo();
+//     s2.getinfo();
+//     return 0;
+// }
+
+
+
+int main(){ // for deep student class
+    deepstudent s1("Manas",8.8);
+    deepstudent s2(s1);
+    s1.getinfo();
+    *(s2.cgpaptr)= 9;
+    s1.getinfo();
+    s2.getinfo();
 }
