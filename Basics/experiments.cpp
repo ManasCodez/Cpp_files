9b2738f5c037397155f090c4afaf7d1b69438cc8
Manas Sharma <manasmailsss@gmail.com>
2025-10-02 21:59:16 +0530

    update

--- Basics/experiments.cpp
+++ Basics/experiments.cpp
@@ -3,8 +3,10 @@
 #include<algorithm>
 #include<unordered_map>
 #include<vector>
+#include<climits>
 using namespace std;
 
 int main(){
-    cout << "manas";
+    //4byte = 32 bit
+    cout << INT_MAX;
 }
