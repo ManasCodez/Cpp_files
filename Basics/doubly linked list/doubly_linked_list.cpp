5f64e5df47983ec9dbb9704fa39664216b9f489e
Manas Sharma <manasmailsss@gmail.com>
2026-01-26 23:46:59 +0530

    Implemented pop_back() and push_back() in DLL

--- Basics/doubly linked list/doubly_linked_list.cpp
+++ Basics/doubly linked list/doubly_linked_list.cpp
@@ -42,6 +42,21 @@ public:
         }
     }
 
+    void push_back(int val)
+    {
+        node *newnode = new node(val);
+        if (head == NULL)
+        {
+            head = tail = newnode;
+        }
+        else
+        {
+            tail->next = newnode;
+            newnode->pre = tail;
+            tail = newnode;
+        }
+    }
+
     void pop_front()
     {
         if (head == NULL)
@@ -52,13 +67,32 @@ public:
         {
             head = tail = NULL;
         }
-        else{
+        else
+        {
             node *temp = head;
             head = head->next;
             head->pre = NULL;
             temp->next = NULL;
             delete temp;
         }
+    }
+
+    void pop_back()
+    {
+        if (head == NULL)
+            return;
+
+        if (head == tail)
+        {
+            delete head;
+            head = tail = NULL;
+            return;
+        }
+
+        node *temp = tail;
+        tail = tail->pre;
+        tail->next = NULL;
+        delete temp;
     }
 
     void print()
@@ -78,7 +112,10 @@ int main()
     DLL ll;
     ll.push_front(1);
     ll.push_front(2);
+    ll.push_back(3);
     ll.print();
     ll.pop_front();
+    ll.print();
+    ll.pop_back();
     ll.print();
 }
