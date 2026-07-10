4fc2180a5532ddcc0f136ecbfa5685e950130c99
Manas <rks@RKSs-MacBook-Air.local>
2026-07-10 20:56:13 +0530

    level order traversal

--- Basics/Binary_tree/level_order_traversal.cpp
+++ Basics/Binary_tree/level_order_traversal.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include<vector>
+#include<queue>
 using namespace std;
 
 
@@ -28,12 +29,54 @@ node* buildtree(vector<int>&pre_seq){
     return root;
 }
 
+//for print in a single line
+// void levelorder(node* root, queue<node*>&q){
+//     q.push(root);
+    
+//     while(q.size()>0){
+//         node* curr = q.front();
+//         q.pop();
+        
+//         cout << curr->data <<' ';
+
+//         if(curr->left !=NULL) q.push(curr->left);
+//         if(curr->right !=NULL) q.push(curr->right);
+//     }
+//     cout << endl;
+// }
+
+
+
+
+//for printing a single level in a single line
+void levelorder(node* root, queue<node*>&q){
+    q.push(root);
+    q.push(NULL);
+    while(q.size()>0){
+        node* curr = q.front();
+        q.pop();
+        if(curr == NULL){
+            if(q.size()>0){
+                cout << endl;
+                q.push(NULL);
+                continue;
+            }
+            else break;
+        }
+        cout << curr->data <<' ';
+
+        if(curr->left !=NULL) q.push(curr->left);
+        if(curr->right !=NULL) q.push(curr->right);
+    }
+    cout << endl;
+}
 
 
 int main(){
-
+    queue<node*>q;
     vector<int>pre_seq= {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
     node* root = buildtree(pre_seq);
-    postorder(root);
+    levelorder(root , q);
+    
 
 }
